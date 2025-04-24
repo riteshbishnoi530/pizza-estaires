21ed6cf441ac3d2b29a808dcf052bcd1d0267cf6
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-04-24 15:06:57 +0530

    add hover on footer icon and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,9 @@ function Footer() {
             <div className='max-w-[1439px] xl:justify-between flex max-xl:flex-wrap mx-auto px-4'>
                 <div className='flex max-xl:flex-col-reverse'>
                     <div className='flex items-center gap-[33px] max-lg:mt-[30px]'>
+                        <Link href="/">
                         <Image src="/assets/images/logo.webp" alt="footer" width={124} height={116} />
+                        </Link>
                         <p className='text-xl lg:hidden'>Pizza Estaires:<Link className='text-lg' href="/">Adresse, téléphone, VAT, SIREN</Link></p>
                     </div>
                     <div className='xl:ml-[120px] flex-wrap flex gap-[91px] max-sm:gap-x-6 max-sm:gap-y-[30px] '>
@@ -17,7 +19,7 @@ function Footer() {
                             <p className='text-2xl font-semibold'>Livraison</p>
                             <div className='flex flex-col gap-5'>
                                 {[...Array(3)].map((_, i) => (
-                                    <Link href="/" key={i} className='text-lg' >59 000 Estaires</Link>
+                                    <Link href="/" key={i} className='text-lg hover:text-black/70 transition-all duration-300 ease-linear' >59 000 Estaires</Link>
                                 ))}
                             </div>
                         </div>
@@ -25,15 +27,15 @@ function Footer() {
                             <p className='text-2xl font-semibold'>Livraison</p>
                             <div className='flex flex-col gap-5'>
                                 {[...Array(3)].map((_, i) => (
-                                    <Link href="/" key={i} className='text-lg' >59 000 Estaires</Link>
+                                    <Link href="/" key={i} className='text-lg hover:text-black/70 transition-all duration-300 ease-linear' >59 000 Estaires</Link>
                                 ))}
                             </div>
                         </div>
                         <div>
                             <p className='text-2xl font-semibold'>Recrutement</p>
                             <div className='flex flex-col gap-5'>
-                                <Link href="/" className='text-lg' >Offer 1</Link>
-                                <Link href="/" className='text-lg' >Offer 2</Link>
+                                <Link href="/" className='text-lg hover:text-black/70 transition-all duration-300 ease-linear' >Offer 1</Link>
+                                <Link href="/" className='text-lg hover:text-black/70 transition-all duration-300 ease-linear' >Offer 2</Link>
                             </div>
                         </div>
                     </div>
@@ -41,11 +43,11 @@ function Footer() {
                 <div className='lg:ml-[91px] max-lg:mt-[30px] max-lg:mr-[91px]'>
                     <p className='text-2xl font-semibold max-lg:hidden'>Réseaux Sociaux</p>
                     <div className='flex gap-[10px]'>
-                        <Link href=""><Facebook /></Link>
-                        <Link href=""><Instagram /></Link>
-                        <Link href=""><Tiktok /></Link>
-                        <Link href=""><Google /></Link>
-                        <Link href=""><Wechat /></Link>
+                        <Link target='_blank' className='hover:scale-105 transition-all duration-300 ease-linear' href="https://facebook.com"><Facebook /></Link>
+                        <Link target='_blank' className='hover:scale-105 transition-all duration-300 ease-linear' href="https://instagram.com"><Instagram /></Link>
+                        <Link target='_blank' className='hover:scale-105 transition-all duration-300 ease-linear' href="https://tiktok.com"><Tiktok /></Link>
+                        <Link target='_blank' className='hover:scale-105 transition-all duration-300 ease-linear' href="https://google.com"><Google /></Link>
+                        <Link target='_blank' className='hover:scale-105 transition-all duration-300 ease-linear' href="https://wechat.com"><Wechat /></Link>
                     </div>
                 </div>
             </div>
